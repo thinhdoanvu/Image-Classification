a012e6e9ab684a4bb3de475e5a07196ea22fbd05
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-03-19 23:35:23 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from PIL import Image
 import torch
 from torchvision.transforms import ToTensor
 import torchvision.models as models
-from torch.utils.data import Dataset, DataLoader
-from torchvision.datasets import ImageFolder
 import numpy as np
 import pandas as pd
 
@@ -20,52 +18,54 @@ def setup_cuda():
 
     return torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+def predict():
+    clf.eval()
+    
+    
+
+    folder_path = './data/test/'
+    # Read the CSV file
+    csv_file_path = './data/data.csv'  # path to your CSV file
+    df = pd.read_csv(csv_file_path)
+
+    # Get the list of image files in the test folder
+    image_files = [file for file in os.listdir(folder_path) if file.endswith('.jpg')]
+    # print(image_files)
+
+    # Iterate over each image file in the test folder
+    for image_file in image_files:
+        # Process each image file here
+        image_path = os.path.join(folder_path, image_file)  # Construct the full path to the image file
+        img = Image.open(image_path)
+        img_tensor = ToTensor()(img).unsqueeze(0).to(device)
+
+        output = clf(img_tensor)
+        predicted_class = torch.argmax(output).item()
+        #print(predicted_class)
+
+        # Search for matching rows based on the image file name
+        rows = df[df['filepaths'].str.endswith(image_file)]
+        if not rows.empty:
+            match_found = False
+            for index, row in rows.iterrows():
+                index_class = row['class index']
+                label = row['labels']
+                if predicted_class == index_class:
+                    print(f"Image: {image_file}, Predicted label: {label}")
+                    match_found = True
+                    break
+            if not match_found:
+                print(f"Image: {image_file}, Predicted class index: {predicted_class}, Index class from CSV: {index_class}")
+        else:
+            print(f"No match found for image: {image_file}")
+
 if __name__ == "__main__":
     device = setup_cuda()
 
-    # Load the model
+    # 2. Create a segmentation model, then load the trained weights
     clf = models.resnet18(pretrained=True).to(device)
-    with open('model_state.pt', 'rb') as f:
-        clf.load_state_dict(torch.load(f))
-    clf.eval()  # Set the model to evaluation mode
+    clf.load_state_dict(torch.load('model_state.pt', device))
+    print('The classify model has been loaded.')
 
-    # Define the device (CPU or GPU) to run the model on
-    clf.to(device)
-    folder_path = './data/test'
-    # Read the CSV file
-    csv_file_path = './data/data.csv'  #path to your CSV file
-    df = pd.read_csv(csv_file_path)
-
-    # Get the list of subfolders (each representing a class)
-    class_folders = [folder for folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, folder))]
-    # print(class_folders)
-
-    # Iterate over each class folder
-    for class_folder in class_folders:
-        # Get the path to the current class folder
-        class_folder_path = os.path.join(folder_path, class_folder)
-        print(class_folder_path)
-        # Get the list of image files in the class folder
-        image_files = [file for file in os.listdir(class_folder_path) if file.endswith('.jpg')]
-        print(image_files)
-
-        # Iterate over each image file in the class folder
-        for image_file in image_files:
-            # Process each image file here
-            # Predict for each image
-            image_path = os.path.join(class_folder_path, image_file)
-            img = Image.open(image_path)
-            img_tensor = ToTensor()(img).unsqueeze(0).to(device)
-
-            # Perform inference
-            with torch.no_grad():
-                output = clf(img_tensor)
-                predicted_class = torch.argmax(output).item()
-
-            row = df[df['filepaths'].str.endswith(image_file)]
-            if not row.empty:
-                # Get the index class from the matched row
-                index_class = row['labels'].values[0]
-                print(f"Image: {image_file}, Predicted class index: {index_class}")
-            else:
-                print(f"No match found for image: {image_file}")
+    # 3. Perform segmentation
+    predict()
