9bcb896582f83b77a8c0b438104551cd0fa5dfb7
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-08-15 18:11:14 +1000

    Add files via upload

---
+++ Coordinate Attention/models/caresnet18.py
@@ -0,0 +1,343 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class SAM(nn.Module):
+    def __init__(self, bias=False):
+        super(SAM, self).__init__()
+        self.bias = bias
+        self.conv = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3, dilation=1,
+                              bias=self.bias)
+
+    def forward(self, x):
+        max = torch.max(x, 1)[0].unsqueeze(1)
+        avg = torch.mean(x, 1).unsqueeze(1)
+        concat = torch.cat((max, avg), dim=1)
+        output = self.conv(concat)
+        output = F.sigmoid(output) * x
+        return output
+
+
+class CAM(nn.Module):
+    def __init__(self, channels, r):
+        super(CAM, self).__init__()
+        self.channels = channels
+        self.r = r
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=self.channels, out_features=self.channels // self.r, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_features=self.channels // self.r, out_features=self.channels, bias=True))
+
+    def forward(self, x):
+        max = F.adaptive_max_pool2d(x, output_size=1)
+        avg = F.adaptive_avg_pool2d(x, output_size=1)
+        b, c, _, _ = x.size()
+        linear_max = self.linear(max.view(b, c)).view(b, c, 1, 1)
+        linear_avg = self.linear(avg.view(b, c)).view(b, c, 1, 1)
+        output = linear_max + linear_avg
+        output = F.sigmoid(output) * x
+        return output
+
+
+class CBAM(nn.Module):
+    def __init__(self, channels, r):
+        super(CBAM, self).__init__()
+        self.channels = channels
+        self.r = r
+        self.sam = SAM(bias=False)
+        self.cam = CAM(channels=self.channels, r=self.r)
+
+    def forward(self, x):
+        output = self.cam(x)
+        output = self.sam(output)
+        return output + x
+
+
+class SEBlock(nn.Module):
+    def __init__(self, input_channels, reduction_ratio=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Linear(input_channels, input_channels // reduction_ratio, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(input_channels // reduction_ratio, input_channels, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        batch_size, channels, _, _ = x.size()
+        y = self.avg_pool(x).view(batch_size, channels)
+        y = self.fc1(y)
+        y = self.relu(y)
+        y = self.fc2(y)
+        y = self.sigmoid(y).view(batch_size, channels, 1, 1)
+        return x * y.expand_as(x)
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)  # origin: 0:batch, 1:chanel, 2:height, 3:width
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+
+
+class CAResNet18(nn.Module):
+    def __init__(self, num_classes):
+        super(CAResNet18, self).__init__()
+
+        self.dropout_percentage = 0.5
+        self.relu = nn.ReLU()
+
+        # BLOCK-1 (starting block) input=(224x224) output=(56x56)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
+        self.batchnorm1 = nn.BatchNorm2d(64)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+
+        # BLOCK-2 (1) input=(56x56) output = (56x56)
+        self.conv2_1_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm2_1_1 = nn.BatchNorm2d(64)
+        self.conv2_1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm2_1_2 = nn.BatchNorm2d(64)
+        self.dropout2_1 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb2_1 = CBAM(64, r=16)
+        # self.se2_1 = SEBlock(64)
+        self.ca2_1 = CoordAtt(64,64, 32)
+
+        # BLOCK-2 (2)
+        self.conv2_2_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm2_2_1 = nn.BatchNorm2d(64)
+        self.conv2_2_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm2_2_2 = nn.BatchNorm2d(64)
+        self.dropout2_2 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb2_2 = CBAM(64, r=16)
+        # self.se2_2 = SEBlock(64)
+        self.ca2_2 = CoordAtt(64, 64, 32)
+
+        # BLOCK-3 (1) input=(56x56) output = (28x28)
+        self.conv3_1_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.batchnorm3_1_1 = nn.BatchNorm2d(128)
+        self.conv3_1_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm3_1_2 = nn.BatchNorm2d(128)
+
+        #concat la lam!!!
+        self.concat_adjust_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), stride=(2, 2),
+                                         padding=(0, 0))
+        self.dropout3_1 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb3_1 = CBAM(128, r=16)
+        # self.se3_1 = SEBlock(128)
+        self.ca3_1 = CoordAtt(128, 128, 32)
+
+        # BLOCK-3 (2)
+        self.conv3_2_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm3_2_1 = nn.BatchNorm2d(128)
+        self.conv3_2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm3_2_2 = nn.BatchNorm2d(128)
+        self.dropout3_2 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb3_2 = CBAM(128, r=16)
+        # self.se3_2 = SEBlock(128)
+        self.ca3_2 = CoordAtt(128, 128, 32)
+
+        # BLOCK-4 (1) input=(28x28) output = (14x14)
+        self.conv4_1_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.batchnorm4_1_1 = nn.BatchNorm2d(256)
+        self.conv4_1_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm4_1_2 = nn.BatchNorm2d(256)
+
+        #danh cho block 42
+        self.concat_adjust_4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(1, 1), stride=(2, 2),
+                                         padding=(0, 0))
+        self.dropout4_1 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb4_1 = CBAM(256, r=16)
+        # self.se4_1 = SEBlock(256)
+        self.ca4_1 = CoordAtt(256, 256, 32)
+
+        # BLOCK-4 (2)
+        self.conv4_2_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm4_2_1 = nn.BatchNorm2d(256)
+        self.conv4_2_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm4_2_2 = nn.BatchNorm2d(256)
+        self.dropout4_2 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb4_2 = CBAM(256, r=16)
+        # self.se4_2 = SEBlock(256)
+        self.ca4_2 = CoordAtt(256, 256, 32)
+
+        # BLOCK-5 (1) input=(14x14) output = (7x7)
+        self.conv5_1_1 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.batchnorm5_1_1 = nn.BatchNorm2d(512)
+        self.conv5_1_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm5_1_2 = nn.BatchNorm2d(512)
+        self.concat_adjust_5 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(1, 1), stride=(2, 2),
+                                         padding=(0, 0))
+        self.dropout5_1 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb5_1 = CBAM(512, r=16)
+        # self.se5_1 = SEBlock(512)
+        self.ca5_1 = CoordAtt(512, 512, 32)
+
+        # BLOCK-5 (2)
+        self.conv5_2_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm5_2_1 = nn.BatchNorm2d(512)
+        self.conv5_2_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.batchnorm5_2_2 = nn.BatchNorm2d(512)
+        self.dropout5_2 = nn.Dropout(p=self.dropout_percentage)
+        # self.cb5_2 = CBAM(512, r=16)
+        # self.se5_2 = SEBlock(512)
+        self.ca5_2 = CoordAtt(512, 512, 32)
+
+        # Final Block input=(7x7)
+        self.avgpool = nn.AvgPool2d(kernel_size=(7, 7), stride=(1, 1))
+        self.fc = nn.Linear(in_features=1 * 1 * 512, out_features=1000)
+        self.out = nn.Linear(in_features=1000, out_features=num_classes)
+        # END
+
+    def forward(self, x):
+        # block 1 --> Starting block
+        x = self.relu(self.batchnorm1(self.conv1(x)))
+        op1 = self.maxpool1(x)
+
+        # block2 - 1
+        x = self.relu(self.batchnorm2_1_1(self.conv2_1_1(op1)))  # conv2_1
+        x = self.batchnorm2_1_2(self.conv2_1_2(x))  # conv2_1
+        x = self.dropout2_1(x)
+        # x = self.cb2_1(x)
+        # x = self.se2_1(x)
+        x = self.ca2_1(x)
+        # block2 - Adjust - No adjust in this layer as dimensions are already same
+        # block2 - Concatenate 1
+        op2_1 = self.relu(x + op1)
+
+        # block2 - 2
+        x = self.relu(self.batchnorm2_2_1(self.conv2_2_1(op2_1)))  # conv2_2
+        x = self.batchnorm2_2_2(self.conv2_2_2(x))  # conv2_2
+        x = self.dropout2_2(x)
+        # x = self.cb2_2(x)
+        # x = self.se2_2(x)
+        x = self.ca2_2(x)
+        # op - block2
+        op2 = self.relu(x + op2_1)
+
+        # block3 - 1[Convolution block]
+        x = self.relu(self.batchnorm3_1_1(self.conv3_1_1(op2)))  # conv3_1
+        x = self.batchnorm3_1_2(self.conv3_1_2(x))  # conv3_1
+        x = self.dropout3_1(x)
+        # x = self.cb3_1(x)
+        # x = self.se3_1(x)
+        x = self.ca3_1(x)
+
+        # block3 - Adjust
+        op2 = self.concat_adjust_3(op2)  # SKIP CONNECTION
+        # block3 - Concatenate 1
+        op3_1 = self.relu(x + op2)
+        # block3 - 2[Identity Block]
+        x = self.relu(self.batchnorm3_2_1(self.conv3_2_1(op3_1)))  # conv3_2
+        x = self.batchnorm3_2_2(self.conv3_2_2(x))  # conv3_2
+        x = self.dropout3_2(x)
+        # x = self.cb3_2(x)
+        # x = self.se3_2(x)
+        x = self.ca3_2(x)
+
+        # op - block3
+        op3 = self.relu(x + op3_1)
+
+        # block4 - 1[Convolition block]
+        x = self.relu(self.batchnorm4_1_1(self.conv4_1_1(op3)))  # conv4_1
+        x = self.batchnorm4_1_2(self.conv4_1_2(x))  # conv4_1
+        x = self.dropout4_1(x)
+        # x = self.cb4_1(x)
+        # x = self.se4_1(x)
+        x = self.ca4_1(x)
+
+        # block4 - Adjust
+        op3 = self.concat_adjust_4(op3)  # SKIP CONNECTION
+        # block4 - Concatenate 1
+        op4_1 = self.relu(x + op3)
+        # block4 - 2[Identity Block]
+        x = self.relu(self.batchnorm4_2_1(self.conv4_2_1(op4_1)))  # conv4_2
+        x = self.batchnorm4_2_2(self.conv4_2_2(x))  # conv4_2
+        x = self.dropout4_2(x)
+        # x = self.cb4_2(x)
+        # x = self.se4_2(x)
+        x = self.ca4_2(x)
+
+        # op - block4
+        op4 = self.relu(x + op4_1)
+
+        # block5 - 1[Convolution Block]
+        x = self.relu(self.batchnorm5_1_1(self.conv5_1_1(op4)))  # conv5_1
+        x = self.batchnorm5_1_2(self.conv5_1_2(x))  # conv5_1
+        x = self.dropout5_1(x)
+        # x = self.cb5_1(x)
+        # x = self.se5_1(x)
+        x = self.ca5_1(x)
+
+        # block5 - Adjust
+        op4 = self.concat_adjust_5(op4)  # SKIP CONNECTION
+        # block5 - Concatenate 1
+        op5_1 = self.relu(x + op4)
+        # block5 - 2[Identity Block]
+        x = self.relu(self.batchnorm5_2_1(self.conv5_2_1(op5_1)))  # conv5_2
+        x = self.batchnorm5_2_2(self.conv5_2_1(x))  # conv5_2
+        x = self.dropout5_2(x)
+        # x = self.cb5_2(x)
+        # x = self.se5_2(x)
+        x = self.ca5_2(x)
+
+        # op - block5
+        op5 = self.relu(x + op5_1)
+
+        # FINAL BLOCK - classifier
+        x = self.avgpool(op5)
+        x = x.reshape(x.shape[0], -1)
+        x = self.relu(self.fc(x))
+        x = self.out(x)
+
+        return x  # USE 4 Classification
+        # return [op1, op2_1, op2, op3_1, op3, op4_1, op4, op5_1, op5]  # ONLY FOR VISUALIZE FEATURE MAPS
