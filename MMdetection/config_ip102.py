46ce504b2c2248e8959b5002019019d1104dbaaf
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-12-20 19:52:43 +0700

    Add files via upload

---
+++ MMdetection/config_ip102.py
@@ -0,0 +1,88 @@
+_base_ = 'faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py'   #faster-rcnn Resnet: 50
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=102)
+        # mask_head=dict(num_classes=102) #only for segmentation
+    )
+)
+
+
+dataset_type = 'CocoDataset'
+
+classes = ('rice leaf roller', 'rice leaf caterpillar', 'paddy stem maggot', 'asiatic rice borer', 'yellow rice borer',
+        'rice gall midge', 'Rice Stemfly', 'brown plant hopper', 'white backed plant hopper', 'small brown plant hopper',
+        'rice water weevil', 'rice leafhopper', 'grain spreader thrips', 'rice shell pest', 'grub', 'mole cricket', 'wireworm',
+        'white margined moth', 'black cutworm', 'large cutworm', 'yellow cutworm', 'red spider', 'corn borer', 'army worm', 'aphids',
+        'Potosiabre vitarsis', 'peach borer', 'english grain aphid', 'green bug', 'bird cherry-oataphid', 'wheat blossom midge',
+        'penthaleus major', 'longlegged spider mite', 'wheat phloeothrips', 'wheat sawfly', 'cerodonta denticornis', 'beet fly',
+        'flea beetle', 'cabbage army worm', 'beet army worm', 'Beet spot flies', 'meadow moth', 'beet weevil', 'sericaorient alismots chulsky',
+        'alfalfa weevil', 'flax budworm', 'alfalfa plant bug', 'tarnished plant bug', 'Locustoidea', 'lytta polita', 'legume blister beetle',
+        'blister beetle', 'therioaphis maculata Buckton', 'odontothrips loti', 'Thrips', 'alfalfa seed chalcid', 'Pieris canidia',
+        'Apolygus lucorum', 'Limacodidae', 'Viteus vitifoliae', 'Colomerus vitis', 'Brevipoalpus lewisi McGregor', 'oides decempunctata',
+        'Polyphagotars onemus latus', 'Pseudococcus comstocki Kuwana', 'parathrene regalis', 'Ampelophaga', 'Lycorma delicatula', 'Xylotrechus',
+        'Cicadella viridis', 'Miridae', 'Trialeurodes vaporariorum', 'Erythroneura apicalis', 'Papilio xuthus', 'Panonchus citri McGregor',
+        'Phyllocoptes oleiverus ashmead', 'Icerya purchasi Maskell', 'Unaspis yanonensis', 'Ceroplastes rubens', 'Chrysomphalus aonidum',
+        'Parlatoria zizyphus Lucus', 'Nipaecoccus vastalor', 'Aleurocanthus spiniferus', 'Tetradacus c Bactrocera minax ', 'Dacus dorsalis(Hendel)',
+        'Bactrocera tsuneonis', 'Prodenia litura', 'Adristyrannus', 'Phyllocnistis citrella Stainton', 'Toxoptera citricidus', 'Toxoptera aurantii',
+        'Aphis citricola Vander Goot', 'Scirtothrips dorsalis Hood', 'Dasineura sp', 'Lawana imitata Melichar', 'Salurnis marginella Guerr',
+        'Deporaus marginatus Pascoe', 'Chlumetia transversa', 'Mango flat beak leafhopper', 'Rhytidodera bowrinii white', 'Sternochetus frigidus',
+        'Cicadellidae')
+
+data_root = 'data/coco/'
+
+
+# Training pipeline
+backend_args = None
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    # dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=False,# Ensure clean worker state
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='images/'),
+        pipeline=train_pipeline  # fit 640x640
+        )
+    )
+
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='images/'),
+        pipeline=test_pipeline  # fit 640x640
+        )
+    )
+
+
+
+# Set the maximum number of epochs for training
+train_cfg = dict(max_epochs=100)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
---
+++ MMdetection/config_r2000.py
@@ -0,0 +1,71 @@
+_base_ = 'faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py'   #faster-rcnn Resnet: 50
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=16)
+        # mask_head=dict(num_classes=102) #only for segmentation
+    )
+)
+
+
+dataset_type = 'CocoDataset'
+
+classes = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15',)
+
+data_root = 'data/coco/'
+
+
+# Training pipeline
+backend_args = None
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    # dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=False,# Ensure clean worker state
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        pipeline=train_pipeline  # fit 640x640
+        )
+    )
+
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='val2017/'),
+        pipeline=test_pipeline  # fit 640x640
+        )
+    )
+
+
+
+# Set the maximum number of epochs for training
+train_cfg = dict(max_epochs=100)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
