202884e8abcc0d957a2a5dcaa6bbe7107b3eccc9
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-12-23 10:50:55 +0700

    Add files via upload

---
+++ MMdetection/retina_ip102.py
@@ -0,0 +1,95 @@
+_base_ = 'retinanet/retinanet_r50_fpn_1x_coco.py'  # Base configuration
+
+# Training parameters
+train_batch_size_per_gpu = 4  # Optimal for RTX 4090
+train_num_workers = 4
+max_epochs = 100
+base_lr = 0.0001
+
+# Model configuration
+model = dict(
+    bbox_head=dict(
+        num_classes=102  # Number of classes in your dataset
+    )
+)
+
+# Dataset settings
+dataset_type = 'CocoDataset'
+
+# List of classes
+classes = (
+    'rice leaf roller', 'rice leaf caterpillar', 'paddy stem maggot', 'asiatic rice borer', 'yellow rice borer',
+    'rice gall midge', 'Rice Stemfly', 'brown plant hopper', 'white backed plant hopper', 'small brown plant hopper',
+    'rice water weevil', 'rice leafhopper', 'grain spreader thrips', 'rice shell pest', 'grub', 'mole cricket', 'wireworm',
+    'white margined moth', 'black cutworm', 'large cutworm', 'yellow cutworm', 'red spider', 'corn borer', 'army worm', 'aphids',
+    'Potosiabre vitarsis', 'peach borer', 'english grain aphid', 'green bug', 'bird cherry-oataphid', 'wheat blossom midge',
+    'penthaleus major', 'longlegged spider mite', 'wheat phloeothrips', 'wheat sawfly', 'cerodonta denticornis', 'beet fly',
+    'flea beetle', 'cabbage army worm', 'beet army worm', 'Beet spot flies', 'meadow moth', 'beet weevil', 'sericaorient alismots chulsky',
+    'alfalfa weevil', 'flax budworm', 'alfalfa plant bug', 'tarnished plant bug', 'Locustoidea', 'lytta polita', 'legume blister beetle',
+    'blister beetle', 'therioaphis maculata Buckton', 'odontothrips loti', 'Thrips', 'alfalfa seed chalcid', 'Pieris canidia',
+    'Apolygus lucorum', 'Limacodidae', 'Viteus vitifoliae', 'Colomerus vitis', 'Brevipoalpus lewisi McGregor', 'oides decempunctata',
+    'Polyphagotars onemus latus', 'Pseudococcus comstocki Kuwana', 'parathrene regalis', 'Ampelophaga', 'Lycorma delicatula', 'Xylotrechus',
+    'Cicadella viridis', 'Miridae', 'Trialeurodes vaporariorum', 'Erythroneura apicalis', 'Papilio xuthus', 'Panonchus citri McGregor',
+    'Phyllocoptes oleiverus ashmead', 'Icerya purchasi Maskell', 'Unaspis yanonensis', 'Ceroplastes rubens', 'Chrysomphalus aonidum',
+    'Parlatoria zizyphus Lucus', 'Nipaecoccus vastalor', 'Aleurocanthus spiniferus', 'Tetradacus c Bactrocera minax', 'Dacus dorsalis(Hendel)',
+    'Bactrocera tsuneonis', 'Prodenia litura', 'Adristyrannus', 'Phyllocnistis citrella Stainton', 'Toxoptera citricidus', 'Toxoptera aurantii',
+    'Aphis citricola Vander Goot', 'Scirtothrips dorsalis Hood', 'Dasineura sp', 'Lawana imitata Melichar', 'Salurnis marginella Guerr',
+    'Deporaus marginatus Pascoe', 'Chlumetia transversa', 'Mango flat beak leafhopper', 'Rhytidodera bowrinii white', 'Sternochetus frigidus',
+    'Cicadellidae'
+)
+
+data_root = 'data/coco/'
+
+# Train data configuration
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    dataset=dict(
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='images/')
+    )
+)
+
+# Validation data configuration
+val_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    dataset=dict(
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='images/')
+    )
+)
+
+# Evaluator configuration
+val_evaluator = dict(
+    ann_file='./data/coco/annotations/instances_val2017.json'
+)
+
+# Optimizer configuration
+optim_wrapper = dict(
+    optimizer=dict(type='SGD', lr=base_lr, momentum=0.9, weight_decay=0.0001)
+)
+
+# Scheduler configuration - overwrite base
+param_scheduler = [
+    dict(type='MultiStepLR', milestones=[8, 11], gamma=0.1)
+]
+
+# save the best model
+default_hooks = dict(
+    checkpoint=dict(
+        type="CheckpointHook",
+        save_best="coco/bbox_mAP",
+        rule="greater"
+    )
+)
+
+# Training configuration
+train_cfg = dict(
+    max_epochs=max_epochs,
+    val_interval=1
+)
