59f798c9c4034fc85b25e37d319e773db0bf2ba8
thinhdoanvu <thinhdv@ntu.edu.vn>
2025-06-09 14:41:41 +0700

    Create config_swin-S_r2000.py

---
+++ MMdetection/config_swin-S_r2000.py
@@ -0,0 +1,99 @@
+'''
+Swin-Tiny	faster_swin_tiny_fpn_1x_16class.py
+Swin-Small	faster_swin_small_fpn_1x_16class.py
+Swin-Base	faster_swin_base_fpn_1x_16class.py
+Swin-Large	faster_swin_large_fpn_1x_16class.py
+
+This is Small version
+'''
+_base_ = 'dynamic_rcnn/dynamic-rcnn_r50_fpn_1x_coco.py'  # Dùng base schedule, runtime từ dynamic rcnn
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_small_patch4_window7_224.pth'
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=96,
+        depths=[2, 2, 18, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[96, 192, 384, 768],
+        out_channels=256,
+        num_outs=5
+    ),
+    roi_head=dict(bbox_head=dict(num_classes=16))
+)
+
+
+dataset_type = 'CocoDataset'
+classes = ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15')
+
+data_root = 'data/coco/'
+
+backend_args = None
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    # dict(type='RandomFlip', prob=0.5),  # bật nếu muốn augment
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=(640, 640), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape', 'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    persistent_workers=False,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=False,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        pipeline=train_pipeline
+    )
+)
+
+val_dataloader = dict(
+    batch_size=4,
+    num_workers=4,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='val2017/'),
+        pipeline=test_pipeline
+    )
+)
+
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook',
+        save_best='coco/bbox_mAP',
+        rule='greater'
+    )
+)
+
+train_cfg = dict(max_epochs=150)
+runner = dict(type='EpochBasedRunner', max_epochs=150)
